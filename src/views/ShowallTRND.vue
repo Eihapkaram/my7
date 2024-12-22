972ea7cee57c53ab8d1d8dfc73b11eb77d31e0aa
Eihap <eihapkaram9090@gmail.com>
2024-12-22 03:46:39 +0200

    test

---
+++ src/views/ShowallTRND.vue
@@ -0,0 +1,285 @@
+<template>
+  <div>
+    <hr />
+    <form
+      id="searchfor"
+      class="d-flex"
+      role="search"
+      @submit.prevent="dosearch()"
+    >
+      <input
+        name="search"
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+  <hr />
+  <div class="contenier-fliud" id="coon">
+    <swiper-slide id="swip" v-for="film in this.films" :key="film.id"
+      ><img id="item" :src="sr + '/' + film.poster_path" />
+      <div class="popupp" id="popup">
+        <div class="coninform">
+          <span class="type">{{ film.original_language }}</span>
+          <span class="riting"
+            >{{ film.vote_average }} <span class="fa fa-star checked"></span
+          ></span>
+          <span class="type2">web-DL</span>
+        </div>
+        <img
+          @click.prevent="trialasync(film.id)"
+          class="btn1"
+          src="../assets/play.png"
+        />
+        <h3 id="movename">{{ film.title }}</h3>
+      </div>
+    </swiper-slide>
+  </div>
+  <hr />
+  <div
+    class="btn-toolbar justify-content-between"
+    role="toolbar"
+    aria-label="Toolbar with button groups"
+  >
+    <div class="btn-group" role="group" aria-label="First group">
+      <button type="button" class="btn btn-outline-secondary">1</button>
+      <button type="button" class="btn btn-outline-secondary">2</button>
+      <button type="button" class="btn btn-outline-secondary">3</button>
+      <button type="button" class="btn btn-outline-secondary">4</button>
+    </div>
+  </div>
+  <hr />
+</template>
+<script>
+import { mapActions, mapState } from "pinia";
+import { mystore } from "@/store/count";
+export default {
+  data() {
+    return {
+      films: "",
+      sr: "https://image.tmdb.org/t/p/w500",
+      searchresalt: "",
+    };
+  },
+  computed: {
+    ...mapState(mystore, ["ALL"]),
+  },
+  methods: {
+    ...mapActions(mystore, ["ALLfun", "getTrialmovie"]),
+    async trialasync(id) {
+      await this.getTrialmovie(id);
+      this.$router.push({ name: "about", params: { filmid: id } });
+    },
+    dosearch() {
+      const search_form = document
+        .getElementById("searchfor")
+        .search.value.toLowerCase();
+      const filterdata = this.films.filter((el) => {
+        return el.title.toLocaleLowerCase().includes(search_form);
+      });
+      this.films = filterdata;
+      console.log(filterdata);
+    },
+  },
+  async mounted() {
+    await this.ALLfun();
+    this.films = this.ALL;
+    window.scroll(0, 0);
+  },
+};
+</script>
+
+<style scoped>
+#head h3 {
+  margin-right: 20px;
+}
+.checked {
+  color: orange;
+}
+.btn-toolbar {
+  position: relative;
+  left: 48rem;
+  width: 200px;
+}
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: transparent;
+  filter: drop-shadow();
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+swiper-slide #item {
+  display: block;
+  width: 10rem;
+  object-fit: cover;
+  gap: 2rem;
+  border-radius: 5px;
+  position: relative;
+}
+.contenier-fliud {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  width: 100%;
+  height: 100%;
+}
+#swip {
+  text-align: center;
+  font-size: 18px;
+  background: transparent;
+  filter: drop-shadow();
+  height: 50%;
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.checked {
+  color: orange;
+}
+
+#swip #item {
+  width: 15rem;
+  object-fit: cover;
+  gap: 2rem;
+  border-radius: 5px;
+  position: relative;
+}
+.btn1 {
+  width: 50px;
+  height: 50px;
+  display: ;
+  cursor: pointer;
+  border: 2px solid orange;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  align-self: center;
+  top: -50px;
+  right: auto;
+  background-color: orange;
+}
+#swip {
+  border-radius: 5px;
+}
+.popupp:hover .btn1 {
+  animation: btnani 2s infinite linear;
+}
+#swip:hover .popupp {
+  display: flex;
+  border-radius: 5px;
+}
+@keyframes btnani {
+  0% {
+    box-shadow: 0 0 0 0 rgba(226, 193, 7, 0.8);
+    opacity: 1;
+  }
+  40% {
+    box-shadow: 0 0 0 0 rgba(226, 193, 7, 0.8);
+    opacity: 0.7;
+  }
+  80% {
+    box-shadow: 0 0 0 50px rgba(226, 193, 7, 0.8);
+    opacity: 0.7;
+  }
+  100% {
+    box-shadow: 0 0 0 0 rgba(226, 193, 7, 0.8);
+    opacity: 0.7;
+  }
+}
+#movename {
+  position: absolute;
+  z-index: 3;
+  color: rgb(255, 250, 250);
+  top: 180px;
+  font-size: large;
+  align-self: center;
+}
+#moveitem {
+  position: absolute;
+  z-index: 3;
+  color: whitesmoke;
+  top: 350px;
+  font-size: small;
+  align-self: center;
+}
+.coninform {
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  position: relative;
+  top: 0px;
+  right: 0px;
+  row-gap: 3px;
+  height: 100%;
+  width: 290px;
+}
+.popupp {
+  display: none;
+  background-color: rgba(0, 0, 0, 70%);
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  position: absolute;
+  right: auto;
+  height: 360px;
+  width: 240px;
+}
+.riting {
+  background-color: rgba(0, 0, 0, 70%);
+  border-radius: 5px;
+  color: beige;
+  width: 80px;
+}
+.type {
+  background-color: rgb(250, 186, 10);
+  border-radius: 5px;
+  color: black;
+  width: 80px;
+  margin-block: 3px;
+}
+.type2 {
+  background-color: rgba(0, 0, 0, 70%);
+  border-radius: 5px;
+  color: beige;
+  width: 80px;
+}
+/*/desktop/*/
+@media (max-width: 1366px) {
+  .btn-toolbar {
+    position: relative;
+    left: 48rem;
+    width: 200px;
+  }
+}
+/*/tablet/*/
+@media (max-width: 991px) {
+  .btn-toolbar {
+    position: relative;
+    left: 330px;
+  }
+}
+/*/mobile/*/
+@media (max-width: 500px) {
+  .btn-toolbar {
+    position: relative;
+    left: 150px;
+    width: 100px;
+  }
+}
+</style>
